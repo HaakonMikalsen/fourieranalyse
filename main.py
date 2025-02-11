ae499e3e20b591ca767e68c2969ad4c13d50ab7b
Hsackboy <hakmikalsen@gmail.com>
2025-02-11 15:58:30 +0100

    it works both ways

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 
 signalLengthSeconds =3
 samplingFreqPerSecond = 1000
-t_vals = np.linspace(0,signalLengthSeconds,samplingFreqPerSecond*signalLengthSeconds)
+t_vals = np.linspace(0,signalLengthSeconds,int(samplingFreqPerSecond*signalLengthSeconds))
 
 
-def signal(amplitude:float, shift:float,freq:float,t_vals:np.ndarray[float])->np.ndarray[float]:
+def signal(amplitude:float, shift:float,freq:float,t_vals:np.ndarray[float],offset = 0)->np.ndarray[float]:
     """Generates datapoints for sinus signal
 
     Args:
@@ -14,14 +14,15 @@ def signal(amplitude:float, shift:float,freq:float,t_vals:np.ndarray[float])->np
         shift (float): shit up and down vertical axsis
         freq (float): freqenci of signal
         t_vals (np.NDArray[float]): numpy array of evenly spaced t values
+        offset () : offsets signal along the horizantal axsis
 
     Returns:
         numpy array contaning x(t)=A*sin(2pi*f*t)+c
     """
-    return amplitude*np.sin(2*np.pi*freq*t_vals)+shift
+    return amplitude*np.sin(2*np.pi*freq*t_vals +offset)+shift
     #can 2 pi be pre calculated and is that faster? 
 
-def exponetialPart(freq:float,t_vals:np.ndarray[float])->np.ndarray[float]:
+def exponetialPartFHat(freq:float,t_vals:np.ndarray[float])->np.ndarray[float]:
     """Returns real and imaginary part of exp(-2*pi*i*f*t) or exp(-w*t)
 
     Args:
@@ -52,10 +53,12 @@ def f_hat(x_of_t, t_vals,scanRangeFreqMin,scanRangeFreqMax):
     imaginary_part = np.zeros(len(t_vals))
     freqencyValues = np.linspace(scanRangeFreqMin, scanRangeFreqMax, len(t_vals))
 
+    N = len(t_vals)
+
     for i, freq in enumerate(freqencyValues):
-        exp_parts = exponetialPart(freq, t_vals)  
-        real_part[i] = np.sum( exp_parts[0] * x_of_t )
-        imaginary_part[i] =np.sum( exp_parts[1] * x_of_t)
+        exp_parts = exponetialPartFHat(freq, t_vals)  
+        real_part[i] = np.sum( exp_parts[0] * x_of_t )/N
+        imaginary_part[i] =np.sum( exp_parts[1] * x_of_t)/N
     
     return np.array([real_part, imaginary_part]), freqencyValues
 
@@ -69,7 +72,7 @@ def calculateMagnitude(forierValues):
     Returns:
         _type_: 1d list with the combined values
     """
-    return np.sqrt(forierValues[0]**2 + forierValues[1]**2)/len(forierValues[0])*2
+    return np.sqrt(forierValues[0]**2 + forierValues[1]**2)
 
 
 def serchForFreq(fData,xdata,cutOffFromMax=0.5):
@@ -84,77 +87,59 @@ def serchForFreq(fData,xdata,cutOffFromMax=0.5):
         _type_:List of freqcensies found
     """
     maxPoint = np.max(xdata)*cutOffFromMax
-    topPoints = []
+    topPointsFreq = []
+    topPointsAmplitude = []
     for i in range(1, len(xdata) - 1):
         if (xdata[i]>xdata[i-1]) and (xdata[i]>xdata[i+1]) and (xdata[i]>maxPoint):
-            topPoints.append(fData[i])
+            topPointsFreq.append(fData[i])
+            topPointsAmplitude.append(xdata[i])
     
-    return np.array(topPoints)
+    return np.array([topPointsFreq,topPointsAmplitude])
 
 
-
-# signal1 = signal(1,0,2,t_vals)
-# signal2 = signal(1,0,5,t_vals)
-# combinedSignal = signal1+signal2
-
-# plt.subplots(2,3)
-
-# plt.subplot(2,3,1)
-# plt.title("Signal 1")
-# plt.plot(t_vals,signal1)
-# plt.grid()
-# plt.axvline(color = "black")
-# plt.axhline(color = "black")
-
-
-# plt.subplot(2,3,2)
-# plt.title("Signal 2")
-# plt.plot(t_vals,signal2)
-# plt.grid()
-# plt.axvline(color = "black")
-# plt.axhline(color = "black")
+def createSignalFromFreq(freqencis,t_vals,ampltudes=[],offsetX=[],offsetY=[]):
+    if len(ampltudes)==0:
+        ampltudes = np.linspace(1,1,len(freqencis))
+    
+    if len(offsetX)==0:
+        offsetX = np.zeros(len(freqencis))
+    
+    if len(offsetY)==0:
+        offsetY = np.zeros(len(freqencis))
+    
+    combinedSignal = np.zeros(len(t_vals))
+    for i in range(len(freqencis)):
+        combinedSignal+=signal(ampltudes[i],offsetY[i],freqencis[i],t_vals,offsetX[i])
+    return combinedSignal
 
 
-# plt.subplot(2,3,3)
-# plt.title("Kobinasjon")
-# plt.plot(t_vals,combinedSignal)
-# plt.grid()
-# plt.axvline(color = "black")
-# plt.axhline(color = "black")
+def exponetialPartF(freq:float,t_vals:np.ndarray[float])->np.ndarray[float]:
+    negativ_angular_frequency_w = 2*np.pi*freq
+    realPart = np.cos(t_vals*negativ_angular_frequency_w)
+    imaginaryPart = np.sin(t_vals*negativ_angular_frequency_w)
+    return np.array([realPart,imaginaryPart])
 
+def f(f_hat_values,freqencyValues,t_vals):
+    f_values_real = np.zeros(len(t_vals))
+    f_values_imaginary = np.zeros(len(t_vals))
 
+    N = len(t_vals)
 
-# forierValues,freqValues = f_hat(combinedSignal,t_vals,0,10)
+      
+    for i, freq in enumerate(freqencyValues):
+        exp_parts = exponetialPartF(freq, t_vals)  
+        f_values_real += exp_parts[0] * f_hat_values[0][i]
+        f_values_imaginary+= exp_parts[1] * f_hat_values[1][i]
 
-# plt.subplot(2,3,4)
-# plt.title("Forier reell del (abs)")
-# plt.plot(freqValues,np.abs(forierValues[0])/len(forierValues[0]))
-# plt.grid()
-# plt.axvline(color = "black")
-# plt.axhline(color = "black")
+    # f_values_real /= N
+    # f_values_imaginary /=N
+    return f_values_real,f_values_imaginary
 
+combinedSignal = createSignalFromFreq([2,3,5,10],t_vals)
+# combinedSignal = createSignalFromFreq([100,500,1000,1777,420],t_vals)
 
-# plt.subplot(2,3,5)
-# plt.title("Forier imaginær del (abs)")
-# plt.plot(freqValues,np.abs(forierValues[1])/len(forierValues[1]))
-# plt.grid()
-# plt.axvline(color = "black")
-# plt.axhline(color = "black")
-
-
-
-# plt.subplot(2,3,6)
-# plt.title("Forier samlet, justert for magnitude")
-# plt.plot(freqValues,calculateMagnitude(forierValues))
-# plt.grid()
-# plt.axvline(color = "black")
-# plt.axhline(color = "black")
-
-
-signalFreq = np.array([2,4,8,50,100,500,1000,1500,2000])
-signalFreq = np.array([2,4,8])
-signals = np.array([signal(1,0,freq,t_vals) for freq in signalFreq])
-combinedSignal = sum(signals)
+# combinedSignal = np.sin(np.pi*2*2*t_vals)
+# combinedSignal += np.sin(np.pi*2*3*t_vals)+1
 
 # forierValues,freqValues = f_hat(combinedSignal,t_vals,0,4000)
 forierValues,freqValues = f_hat(combinedSignal,t_vals,0,15)
@@ -162,17 +147,28 @@ forierValues,freqValues = f_hat(combinedSignal,t_vals,0,15)
 fourierMagnitudeData = calculateMagnitude(forierValues)
 foundFreqVals = serchForFreq(freqValues,fourierMagnitudeData)
 # print(foundFreqVals)
-print(f"Found {len(foundFreqVals)} frequnceis")
-for freqFound in foundFreqVals:
-    print(f"Found frequce: {freqFound}Hz")
-
+print(f"Found {len(foundFreqVals[0])} frequnceis")
+for i in range(len(foundFreqVals[0])):
+    print(f"Found frequce: {foundFreqVals[0][i]}Hz with amplitude {foundFreqVals[1][i]}")
 plt.title("Forier samlet, justert for magnitude")
 plt.plot(freqValues,fourierMagnitudeData)
+# plt.plot(t_vals,combinedSignal)
 plt.grid()
 plt.axvline(color = "black")
 plt.axhline(color = "black")
 
+plt.show()
 
 
 
+reversed_transfor = f(forierValues,freqValues,t_vals)
+adjust = np.average(np.abs(combinedSignal))/np.average(np.abs(reversed_transfor[0])) 
+
+plt.subplot(1,3,1)
+plt.plot(t_vals,reversed_transfor[0]*adjust)
+plt.subplot(1,3,2)
+plt.plot(t_vals,combinedSignal,color="red")
+plt.subplot(1,3,3)
+plt.plot(t_vals,combinedSignal,color="red")
+plt.plot(t_vals,reversed_transfor[0]*adjust)
 plt.show()
