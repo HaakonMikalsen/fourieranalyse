349f5c5962b945854fd73ff5eb1ae61edaa70f0d
Hsackboy <hakmikalsen@gmail.com>
2025-03-03 11:52:40 +0100

    Func for searching only for whole number freq and heart data and live ploting

--- test.py
+++ test.py
@@ -1,62 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-signalLengthSeconds =3
-samplingFreqPerSecond = 1000
-t_vals = np.linspace(0,signalLengthSeconds,int(samplingFreqPerSecond*signalLengthSeconds))
 
-
-def signal(amplitude:float, shift:float,freq:float,t_vals:np.ndarray[float],offset = 0)->np.ndarray[float]:
+def signal(amplitude:float, shift:float,freq:float,tData:np.ndarray[float],offset = 0)->np.ndarray[float]:
     """Generates datapoints for sinus signal
 
     Args:
         amplitude (float): Amplitude for signal
         shift (float): shit up and down vertical axsis
         freq (float): freqenci of signal
-        t_vals (np.NDArray[float]): numpy array of evenly spaced t values
+        tData (np.NDArray[float]): numpy array of evenly spaced t values
         offset () : offsets signal along the horizantal axsis
 
     Returns:
         numpy array contaning x(t)=A*sin(2pi*f*t)+c
     """
-    return amplitude*np.sin(2*np.pi*freq*t_vals +offset)+shift
-    #can 2 pi be pre calculated and is that faster? 
+    return amplitude*np.sin(2*np.pi*freq*tData +offset)+shift
+    
 
-def exponetialPartFHat(freq:float,t_vals:np.ndarray[float])->np.ndarray[float]:
+def exponetialPartFHat(freq:float,tData:np.ndarray[float])->np.ndarray[float]:
     """Returns real and imaginary part of exp(-2*pi*i*f*t) or exp(-w*t)
 
     Args:
         freq (float): frequancy
-        t_vals (np.NDArray[float]): t values
+        tData (np.NDArray[float]): t values
 
     Returns:
         np.NDArray[float]: 2 d numpy array index 0 real part, index 1 imaginary part
     """
     negativ_angular_frequency_w = -2*np.pi*freq
-    realPart = np.cos(t_vals*negativ_angular_frequency_w)
-    imaginaryPart = np.sin(t_vals*negativ_angular_frequency_w)
+    realPart = np.cos(tData*negativ_angular_frequency_w)
+    imaginaryPart = np.sin(tData*negativ_angular_frequency_w)
     return np.array([realPart,imaginaryPart])
 
-def f_hat(x_of_t, t_vals,scanRangeFreqMin,scanRangeFreqMax):
+def f_hat(x_of_t, tData,scanRangeFreqMin,scanRangeFreqMax):
     """f hat or fourier transformation takes a signal x(t) and converts it to the freq domain
 
     Args:
         x_of_t (_type_): signal
-        t_vals (_type_): time value
+        tData (_type_): time value
         scanRangeFreqMin (_type_): minimum freq
         scanRangeFreqMax (_type_): maximum freq
 
     Returns:
         _type_: index[0][0] real part of transformation, index[0][1] imaginary part of transformation, index[1] frqensy values
     """
-    real_part = np.zeros(len(t_vals))
-    imaginary_part = np.zeros(len(t_vals))
-    freqencyValues = np.linspace(scanRangeFreqMin, scanRangeFreqMax, len(t_vals))
+    real_part = np.zeros(len(tData))
+    imaginary_part = np.zeros(len(tData))
+    freqencyValues = np.linspace(scanRangeFreqMin, scanRangeFreqMax, len(tData))
 
-    N = len(t_vals)
+    N = len(tData)
 
     for i, freq in enumerate(freqencyValues):
-        exp_parts = exponetialPartFHat(freq, t_vals)  
+        exp_parts = exponetialPartFHat(freq, tData)  
         real_part[i] = np.sum( exp_parts[0] * x_of_t )/N
         imaginary_part[i] =np.sum( exp_parts[1] * x_of_t)/N
     
@@ -97,7 +93,7 @@ def serchForFreq(fData,xdata,cutOffFromMax=0.5):
     return np.array([topPointsFreq,topPointsAmplitude])
 
 
-def createSignalFromFreq(freqencis,t_vals,ampltudes=[],offsetX=[],offsetY=[]):
+def createSignalFromFreq(freqencis,tData,ampltudes=[],offsetX=[],offsetY=[]):
     if len(ampltudes)==0:
         ampltudes = np.linspace(1,1,len(freqencis))
     
@@ -107,91 +103,75 @@ def createSignalFromFreq(freqencis,t_vals,ampltudes=[],offsetX=[],offsetY=[]):
     if len(offsetY)==0:
         offsetY = np.zeros(len(freqencis))
     
-    combinedSignal = np.zeros(len(t_vals))
+    xData = np.zeros(len(tData))
     for i in range(len(freqencis)):
-        combinedSignal+=signal(ampltudes[i],offsetY[i],freqencis[i],t_vals,offsetX[i])
-    return combinedSignal
+        xData+=signal(ampltudes[i],offsetY[i],freqencis[i],tData,offsetX[i])
+    return xData
 
 
-def exponetialPartF(freq:float,t_vals:np.ndarray[float])->np.ndarray[float]:
+def exponetialPartF(freq:float,tData:np.ndarray[float])->np.ndarray[float]:
     negativ_angular_frequency_w = 2*np.pi*freq
-    realPart = np.cos(t_vals*negativ_angular_frequency_w)
-    imaginaryPart = np.sin(t_vals*negativ_angular_frequency_w)
+    realPart = np.cos(tData*negativ_angular_frequency_w)
+    imaginaryPart = np.sin(tData*negativ_angular_frequency_w)
     return np.array([realPart,imaginaryPart])
 
-def f(f_hat_values,freqencyValues,t_vals):
-    f_values_real = np.zeros(len(t_vals))
-    f_values_imaginary = np.zeros(len(t_vals))
+def f(f_hat_values,freqencyValues,tData):
+    f_values_real = np.zeros(len(tData))
+    f_values_imaginary = np.zeros(len(tData))
 
-    N = len(t_vals)
+    N = len(tData)
 
       
     for i, freq in enumerate(freqencyValues):
-        exp_parts = exponetialPartF(freq, t_vals)  
+        exp_parts = exponetialPartF(freq, tData)  
         f_values_real += exp_parts[0] * f_hat_values[0][i]
         f_values_imaginary+= exp_parts[1] * f_hat_values[1][i]
 
-    # f_values_real /= N
-    # f_values_imaginary /=N
+    
+    
     return f_values_real,f_values_imaginary
 
 
 
-def f_hat_whole_freq(x_of_t, t_vals,scanRangeFreqMin,scanRangeFreqMax):
-    """f hat or fourier transformation takes a signal x(t) and converts it to the freq domain
-
-    Args:
-        x_of_t (_type_): signal
-        t_vals (_type_): time value
-        scanRangeFreqMin (_type_): minimum freq
-        scanRangeFreqMax (_type_): maximum freq
-
-    Returns:
-        _type_: index[0][0] real part of transformation, index[0][1] imaginary part of transformation, index[1] frqensy values
-    """
-    spectrumLen = scanRangeFreqMax-scanRangeFreqMin+1
-    real_part = np.zeros(spectrumLen)
-    imaginary_part = np.zeros(spectrumLen)
-    freqencyValues = np.linspace(scanRangeFreqMin, scanRangeFreqMax, spectrumLen)
-    print(freqencyValues)
-
-    N = len(t_vals)
-    for i in tqdm.trange(len(freqencyValues), desc="Processing"):
-        freq = freqencyValues[i]
-        exp_parts = exponetialPartFHat(freq, t_vals)  
-        real_part[i] = np.sum( exp_parts[0] * x_of_t )/N
-        imaginary_part[i] =np.sum( exp_parts[1] * x_of_t)/N
-    
-    return np.array([real_part, imaginary_part]), freqencyValues
 
 
 
 
 
+timeStepInSeconds =20e-3
+
+filePath = r"./heartData1.txt"
+
+file = open(filePath)
+
+tData =[]
+xData =[]
+
+for i,line in enumerate(file):
+    tData.append(i*timeStepInSeconds)
+    xData.append(int(line))
+
+plotWidthInSeconds = 3
+plotWidthInSteps = int(plotWidthInSeconds/timeStepInSeconds)
+
+tData = np.array(tData)
+xData = np.array(xData)
 
 
-combinedSignal = createSignalFromFreq([2,3,5,10],t_vals)
-# combinedSignal = np.zeros(len(t_vals)) -1
-# half = int(len(t_vals)/2)
-# for i in range(half):
-#     combinedSignal[i+half] =1
-# combinedSignal = createSignalFromFreq([100,500,1000,1777,420],t_vals)
 
-# combinedSignal = np.sin(np.pi*2*2*t_vals)
-# combinedSignal += np.sin(np.pi*2*3*t_vals)+1
 
-# forierValues,freqValues = f_hat(combinedSignal,t_vals,0,4000)
-forierValues,freqValues = f_hat(combinedSignal,t_vals,0,15)
+
+forierValues,freqValues = f_hat(xData,tData,5,35)
 
 fourierMagnitudeData = calculateMagnitude(forierValues)
 foundFreqVals = serchForFreq(freqValues,fourierMagnitudeData)
-# print(foundFreqVals)
+
 print(f"Found {len(foundFreqVals[0])} frequnceis")
 for i in range(len(foundFreqVals[0])):
     print(f"Found frequce: {foundFreqVals[0][i]}Hz with amplitude {foundFreqVals[1][i]}")
 plt.title("Forier samlet, justert for magnitude")
 plt.plot(freqValues,fourierMagnitudeData)
-# plt.plot(t_vals,combinedSignal)
+
 plt.grid()
 plt.axvline(color = "black")
 plt.axhline(color = "black")
@@ -200,14 +180,25 @@ plt.show()
 
 
 
-reversed_transfor = f(forierValues,freqValues,t_vals)
-adjust = np.average(np.abs(combinedSignal))/np.average(np.abs(reversed_transfor[0])) 
+reversed_transfor = f(forierValues,freqValues,tData)
+adjust = np.average(np.abs(xData))/np.average(np.abs(reversed_transfor[0])) 
 
 plt.subplot(1,3,1)
-plt.plot(t_vals,reversed_transfor[0]*adjust)
+plt.plot(tData,reversed_transfor[0]*adjust)
 plt.subplot(1,3,2)
-plt.plot(t_vals,combinedSignal,color="red")
+plt.plot(tData,xData,color="red")
 plt.subplot(1,3,3)
-plt.plot(t_vals,combinedSignal,color="red")
-plt.plot(t_vals,reversed_transfor[0]*adjust)
+plt.plot(tData,xData,color="red")
+plt.plot(tData,reversed_transfor[0]*adjust)
+plt.show()
+
+
+xDataNormalized = (reversed_transfor[0]-np.median(reversed_transfor[0]))/(np.percentile(reversed_transfor[0],75)-np.percentile(reversed_transfor[0],25))
+
+
+for i in range(len(reversed_transfor[0])):
+    plt.clf()
+    plt.plot(tData[i:(i+plotWidthInSteps)], xDataNormalized[i:(i+plotWidthInSteps)]*adjust,color="red")
+    plt.pause(timeStepInSeconds)
+
 plt.show()
